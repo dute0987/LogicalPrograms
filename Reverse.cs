871ae378d1addf0150a32f35aa39c241f1857ece
dute0987 <dute0987@gmail.com>
2022-02-23 21:17:47 +0530

    Main File For Reverse Numbers

---
+++ Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Reverse
+    {
+        public static void ReverseTheNumber()
+        {
+            int n, reverse = 0, rem;
+
+            Console.WriteLine("Enter the Numver");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while ( n != 0 )
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+
+            }
+
+            Console.WriteLine("Reversed Number " + reverse);
+        }
+    }
+}

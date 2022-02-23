a74cc59f1ae64d482f50c66508c9d8c59732fa2f
dute0987 <dute0987@gmail.com>
2022-02-23 21:02:15 +0530

    Program.cs for Perfect Number

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace LogicalProgramming
         public static void Main(string[]args)
         {
             //Console.WriteLine("Welcome to Logical Programs");
-              Fibonacci.DisplayFibonacci()
+            //Fibonacci.DisplayFibonacci()
             //Reverse.ReverseTheNumber();
-            //PerfectNumber.FindThePerfectNumber();
+              PerfectNumber.FindThePerfectNumber();
         }
     }
 }

652f1d5a7455807538433f53ff17dec30e8d8b2f
dute0987 <dute0987@gmail.com>
2022-02-23 20:56:20 +0530

    Greeting message for Logical Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LogicalProgramming
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+              Console.WriteLine("Welcome to Logical Programs");
+            //Fibonacci.DisplayFibonacci()
+            //Reverse.ReverseTheNumber();
+            //PerfectNumber.FindThePerfectNumber();
+        }
+    }
+}
